47449889bf49309be1fe793955e6e4f14b2a6f22
aindrajaya <arista.indrajaya@gmail.com>
2020-04-21 21:24:57 +0700

    change port

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var http = require("http");
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-type': 'Text/plain'});
     res.end('Yuhuuuuu\n');
-}).listen(8080);
+}).listen(8084);
 
-console.log('server runnint at http://127.0.0.1:8080');
+console.log('server runnint at http://127.0.0.1:8084');
 
 process.on('SIGINT', function() {
     process.exit();

14023ee5387419d135e6fdcf80d6e6205b042cf5
aindrajaya <arista.indrajaya@gmail.com>
2020-04-22 13:52:01 +0700

    app.js edited

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-type': 'Text/plain'});
-    res.end('Yuhuuuuu\n');
+    res.end('Njayyyyhuuuuu\n');
 }).listen(8080);
 
 console.log('server runnint at http://localhost:8080');

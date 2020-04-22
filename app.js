a2f087184441d3ed3ca91951d63da3ccd6c059a5
aindrajaya <arista.indrajaya@gmail.com>
2020-04-22 13:40:13 +0700

    edited app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var http = require("http");
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-type': 'Text/plain'});
     res.end('Yuhuuuuu\n');
-}).listen(8084);
+}).listen(8080);
 
-console.log('server runnint at http://localhost:8084');
+console.log('server runnint at http://localhost:8080');
 
 process.on('SIGINT', function() {
     process.exit();
